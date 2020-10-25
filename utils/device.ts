c15e509a5364a2b48103c55744c4b42164a81be2
ardeshir.valipoor <ardeshir.valipoor@helloprint.com>
2020-10-25 20:51:50 +0330

    device detect changed

--- utils/device.ts
+++ utils/device.ts
@@ -1,10 +1,5 @@
-export function OS() {
-    var userAgent = navigator.userAgent || navigator.vendor
-    if (/android/i.test(userAgent)) {
-        return 'Android'
-    }
-    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-        return 'iOS'
-    }
-    return 'others'
-}
+const userAgent = navigator.userAgent || navigator.vendor
+
+export const isIOS = /iPad|iPhone|iPod/.test(userAgent) && !window.MSStream
+export const isAndroid = /android/i.test(userAgent)
+export const otherOS = !isIOS && !isAndroid
