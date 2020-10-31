75555deb57b02c072f3d352efdc89a87a6f02e58
ardeshir.valipoor <ardeshir.valipoor@helloprint.com>
2020-10-31 19:29:10 +0330

    ghost updated

--- components/advanced/ghost/ghost.ts
+++ components/advanced/ghost/ghost.ts
@@ -1,3 +1,4 @@
+import { EASE, HIDE, ROUND, S, WH } from '../../../helpers/style'
 import { Self } from '../../self'
 
 export const Ghost = (options: IGhostOptions = {}) => {
@@ -6,45 +7,38 @@ export const Ghost = (options: IGhostOptions = {}) => {
 
     const opts = { opacity: .15, bg: '#000', size: 48, ...options }
     self.cssClass({
-        width: opts.size + 'px', // Todo: handle %
-        height: opts.size + 'px', // Todo: handle %
-        opacity: '0',
-        pointerEvents: 'none',
-        borderRadius: '50%',
+        ...HIDE,
+        ...WH(opts.size),
+        ...ROUND,
         backgroundColor: opts.bg,
         position: 'absolute',
-        willChange: 'transform,opacity'
+        willChange: 'transform,opacity',
+        transformOrigin: 'center',
+        pointerEvents: 'none'
     })
     let touchStartTime = 0
-    let X = 0
-    let Y = 0
-    self.mounted = () => {
-        const { left, top } = self.el.getBoundingClientRect()
-        X = left
-        Y = top
-    }
+
     return {
         ...self,
-        activate(e: TouchEvent) {
+        activate(x: number, y: number) {
             touchStartTime = new Date().valueOf()
-            const { pageX, pageY } = e.touches[0]
             self.style({
-                transition: 'all 0s',
-                left: pageX - opts.size / 2 + 'px',
-                top: pageY - opts.size / 2 + 'px',
-                transform: `scale(0)`
+                ...EASE(0),
+                ...S(0),
+                left: x - opts.size / 2 + 'px',
+                top: y - opts.size / 2 + 'px',
             })
             self.style({
-                transition: 'all .16s',
-                transform: `scale(2)`,
+                ...EASE(.16),
+                ...S(2),
                 opacity: opts.opacity + ''
-            }, { delay: 5 })
+            }, 10)
         },
         deactivate() {
             self.style({
-                transition: 'all .44s',
-                opacity: '0'
-            }, { delay: 300 - new Date().valueOf() + touchStartTime })
+                ...EASE(.5),
+                ...HIDE,
+            }, 500 - new Date().valueOf() + touchStartTime)
         }
     }
 }
