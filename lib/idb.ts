778bed90c0be7fbec53ccb325c134c640fcec343
ardeshir.valipoor <ardeshir.valipoor@helloprint.com>
2020-12-19 23:19:43 +0330

    more functionality added

--- lib/idb.ts
+++ lib/idb.ts
@@ -1,3 +1,13 @@
+interface IGetAllOptions {
+    skip: number
+    limit: number
+    index?: string // comma separated
+    value?: any // comma separated
+    reverse?: boolean,
+    upperBound?: any,
+    lowerBound?: any
+}
+
 export default {
     // static db: IDBDatabase // Todo: multiple dbs
 
@@ -17,15 +27,20 @@ export default {
             request.onerror = error => reject(error)
         })
     },
-    createStore(dbName: string, name: string, keyPath = 'id', autoIncrement = true, version?: number) {
+    createStore(dbName: string, name: string, version: number, options?: { keyPath: string, autoIncrement?: boolean, indices?: string[] }) {
+        const opts = { keyPath: 'id', autoIncrement: true, indices: [], ...options }
+        // keyPath = 'id', autoIncrement = true
         return new Promise((resolve, reject) => {
             const request = indexedDB.open(dbName, version)
-            request.onupgradeneeded = (event: any) => {
-                const { objectStoreNames, version } = request.result
+            request.onupgradeneeded = (event) => {
+                const db = event?.target?.result
+                const { objectStoreNames } = db
                 if (!Object.values(objectStoreNames).includes(name)) {
-                    request.result.createObjectStore(name, { keyPath, autoIncrement })
+                    const { keyPath, autoIncrement } = opts
+                    const os = db.createObjectStore(name, { keyPath, autoIncrement })
+                    opts.indices.forEach(index => os.createIndex(index, index, { unique: false }))
                 }
-                request.result.close()
+                db.close()
                 return resolve(event)
             }
             request.onsuccess = (event: any) => {
@@ -35,16 +50,14 @@ export default {
             request.onerror = error => reject(error)
         })
     },
-    async add(dbName: string, store: string, object: any, version?: number) {
+    async save(dbName: string, store: string, object: any, version?: number) {
         return new Promise((resolve, reject) => {
             const request = indexedDB.open(dbName, version)
             request.onsuccess = () => {
                 const transaction = request.result.transaction(store, 'readwrite').objectStore(store).add(object)
                 transaction.onsuccess = (successEvent: Event) => {
                     request.result.close()
-                    console.log({ successEvent })
-
-                    return resolve(successEvent.target.result)
+                    return resolve(successEvent?.target?.result)
                 }
                 transaction.onerror = (err) => {
                     return reject(err)
@@ -55,13 +68,11 @@ export default {
             }
         })
     },
-    async byId(dbName: string, store: string, id: any, version?: number) {
-        console.log('inside by ud')
-
+    async get(dbName: string, store: string, id: any, version?: number) {
         return new Promise((resolve, reject) => {
             const request = indexedDB.open(dbName, version)
             request.onsuccess = () => {
-                const reader = request.result.transaction([store]).objectStore(store).get(+id)
+                const reader = request.result.transaction([store]).objectStore(store).get(id)
                 reader.onerror = (err) => {
                     return reject(err)
                 }
@@ -78,17 +89,42 @@ export default {
             }
         })
     },
-    readAll(dbName: string, store: string, version = 1) {
+    // Count
+    all(dbName: string, store: string, version?: number, options?: IGetAllOptions) {
+        const { skip = 0, limit = 0 } = options || {}
         return new Promise<any[]>((resolve, reject) => {
             const request = indexedDB.open(dbName)
             request.onsuccess = (e: Event | any) => {
                 let results: any[] = []
+                let hasSkipped = false
                 const transaction = request.result.transaction([store], 'readonly')
-                transaction.objectStore(store).openCursor().onsuccess = (event: any) => {
+                const os = transaction.objectStore(store)
+
+                let cursorRequest: IDBRequest<IDBCursorWithValue | null>
+
+                if (options?.index && options?.value) {
+                    const index = os.index(options.index)
+                    // const cr = index.count(options?.value)
+                    // cr.onsuccess = (f) => console.log(store, options, cr.result)
+                    const keyRng = options.upperBound ? IDBKeyRange.upperBound(options.value) : IDBKeyRange.only(options.value)
+                    cursorRequest = index.openCursor(keyRng, options.reverse ? 'prev' : 'next')
+                } else {
+                    cursorRequest = os.openCursor(null, options?.reverse ? 'prev' : 'next') //nextunique
+                }
+                cursorRequest.onsuccess = (event: any) => {
                     const cursor = event.target.result
+                    if (!hasSkipped && skip > 0) {
+                        hasSkipped = true
+                        cursor.advance(skip)
+                        return
+                    }
                     if (cursor) {
                         results.push(cursor.value)
-                        cursor.continue()
+                        if (results.length < limit) {
+                            cursor.continue()
+                        } else {
+                            return resolve(results)
+                        }
                     }
                     else {
                         request.result.close()
@@ -104,7 +140,7 @@ export default {
             }
         })
     },
-    query() { },
+    count() { },
 
     delete(dbName: string, store: any, id: any, version = 1) {
         return new Promise((resolve, reject) => {
@@ -121,6 +157,8 @@ export default {
         })
     },
     update(dbName: string, store: any, payload: any, version = 1) {
+        console.log('UPDATE', payload)
+
         return new Promise((resolve, reject) => {
             const request = indexedDB.open(dbName)
             request.onsuccess = (e) => {
