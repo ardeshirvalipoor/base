938913fdd502bb0ebc96c812136479007ccb6950
Ardeshir Valipoor <ardeshir.valipoor@gmail.com>
2020-12-21 21:00:41 +0330

    dropdown fix

--- components/advanced/dropdown/dropdown-item.ts
+++ components/advanced/dropdown/dropdown-item.ts
@@ -1,7 +1,11 @@
 import { Div } from '../../native/div'
 
 export const DropdownItem = (item: any = {}) => {
-
+    if (typeof item == 'string') {
+        item = {
+            title: item, value: item
+        }
+    }
     const self = Div(item.title || item.value)
     self.el.onclick = () => self.emit('item-selected', item)
     self.cssClass({
