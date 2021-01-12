0aadc262160a6a041dd79fc2970b75e1fbce4347
ardeshir.valipoor <ardeshir.valipoor@helloprint.com>
2021-01-12 15:07:59 +0330

    get safe margins

--- utils/device.ts
+++ utils/device.ts
@@ -1,7 +1,28 @@
-const userAgent = navigator.userAgent || navigator.vendor
+const userAgent = window.navigator?.userAgent?.toLowerCase() || window.navigator?.vendor?.toLowerCase()
 
-export const isIOS = /iPad|iPhone|iPod/.test(userAgent) && !window.MSStream
+export const isIOS = /iphone|ipad|ipod/.test(userAgent) && !window.MSStream
 export const isAndroid = /android/i.test(userAgent)
 export const otherOS = !isIOS && !isAndroid
+export const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
 
 export const H = Math.max(window.innerHeight, window.outerHeight) + (isIOS ? 20 : 0)
+export const ST = parseInt(getComputedStyle(document.documentElement).getPropertyValue("--sat")) || 0
+export const SB = parseInt(getComputedStyle(document.documentElement).getPropertyValue("--sab")) || 0
+export function SAFE() {
+    var result, computed, div = document.createElement('div')
+
+    div.style.padding = 'env(safe-area-inset-top) env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left)'
+    document.body.appendChild(div)
+    computed = getComputedStyle(div)
+    result = {
+        top: parseInt(computed.paddingTop) || 0,
+        right: parseInt(computed.paddingRight) || 0,
+        bottom: parseInt(computed.paddingBottom) || 0,
+        left: parseInt(computed.paddingLeft) || 0
+    }
+    document.body.removeChild(div)
+
+    return result
+}
+
+// return { top: 0, right: 44, bottom: 21, left: 44 }
