27072e618e7c57b5a6682ef702673f1e5e5710e7
Ardeshir Valipoor <ardeshir.valipoor@gmail.com>
2022-08-06 16:37:37 +0430

    add formatter

--- helpers/format.ts
+++ helpers/format.ts
@@ -1,3 +1,3 @@
 import { NUMBERS_DELIMITER_REGEX } from "./regex";
 
-export const fformat = (v: number, options : any = {}) => v.toFixed(options.fix || 0).replace(NUMBERS_DELIMITER_REGEX, ',')
+export const fformat = (v: number, options : any = {}) => v.toFixed(v < 10 ? 3: 0).replace(NUMBERS_DELIMITER_REGEX, ',')
