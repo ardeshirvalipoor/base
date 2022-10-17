26b720d5547422d3189ad741f784a6cf265abc82
Ardeshir Valipoor <ardeshir.valipoor@helloprint.com>
2022-10-17 23:24:09 +0200

    need some change

--- utils/texter.ts
+++ utils/texter.ts
@@ -2,6 +2,7 @@ import { IBaseComponent } from '../components/base'
 
 export function texter(base: IBaseComponent<any>) {
     return {
+        // Todo: use text node, not innerHTML... ghostify not working with it
         text(content = '') {
             base.el.textContent = content
         },
