21d4c92eece1a076d8e2ca28f9fd0cf0fcda3c88
Ardeshir Valipoor <ardeshir.valipoor@helloprint.com>
2022-10-23 12:28:03 +0200

    fix add theme performance

--- components/base.ts
+++ components/base.ts
@@ -5,15 +5,14 @@ import styler, { IStyler } from '../utils/styler'
 import emitter, { IEmitter, _emitter } from '../utils/emitter'
 import ldb from '../lib/ldb'
 
-const THEME = ldb.get('BASE_APP_THEME') || 'light'
-console.log('-');
+const THEME = ldb.get('BASE_APP_THEME')
 
 export function Base<K extends keyof HTMLElementTagNameMap>(name = <K>'div'): IBaseComponent<K> {
 
     const id = nextId()
     const el = document.createElement(<K>name); el.setAttribute('data-base-id', id)
     const base = <IBaseComponent<K>>{ id, el }
-    base.el.classList.toggle(THEME)
+    if (THEME) base.el.classList.toggle(THEME)
     emitter.on('theme-changed', (theme: string) => base.el.classList.toggle(theme))
     return Object.assign(base, _emitter(), appender(base), styler(base))
 }
