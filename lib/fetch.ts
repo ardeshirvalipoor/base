b5bd3c64411dddaba18e70f9b6bfda7875df1e8a
Ardeshir Valipoor <ardeshir.valipoor@gmail.com>
2022-11-24 15:51:32 +0100

    fetch updated

--- lib/fetch.ts
+++ lib/fetch.ts
@@ -1,35 +1,25 @@
 // import { DEFAULT_TTL } from "../../configs/ttl"
 import { IXHROptoins, XHR } from "./xhr"
 
-let _cache: any = {}
 let _queue: any = {}
 
 const queuer = async (key: string, url: string, options: IXHROptoins = {}, cb: Function) => {
     if (!_queue[key]) {
-        XHR.get(url, options).then(online => {
-            _cache[key] = online
-            setTimeout(() => {
-                delete _cache[key]
-            }, 2000); // Todo: fix later
-            _queue[key].map((cb: Function) => cb(_cache[key]))
-            delete _queue[key]
-        })
         _queue[key] = []
+        const response = await XHR.get(url, options)
+        _queue[key].map((cb: Function) => cb(response))
+        delete _queue[key]
     }
     _queue[key].push(cb)
 }
 
-/* 
-Fetch is used to queue requests... sometimes to a single url there are many duplicated requests
-*/
 export const fetch = <T>(key: string, url: string = key, options: IXHROptoins = {}) => {
     return new Promise<T>(async (resolve, reject) => {
         try {
-            if (_cache[key]) return resolve(_cache[key])
             queuer(key, url, options, (r: T) => resolve(r))
         } catch (error) {
             console.log(key, url, error)
-            return resolve([] as T)
+            return reject(error)
         }
     })
 }
