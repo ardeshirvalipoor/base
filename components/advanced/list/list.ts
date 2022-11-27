28c2ca0fdf2db05d9d15ee46fe53f8d7320bb7c8
Ardeshir Valipoor <ardeshir.valipoor@gmail.com>
2022-11-27 18:09:46 +0100

    scroll changed

--- components/advanced/list/list.ts
+++ components/advanced/list/list.ts
@@ -12,10 +12,10 @@ export function List<U>() {
     base.el.addEventListener('scroll', () => {
         base.emit('scroll', base.el.scrollTop)
         if (base.el.scrollHeight <= base.el.scrollTop + height) {
-            base.emit('scrolled-to-end')
+            base.emit('scrolled-end')
         }
         if (base.el.scrollTop == 0) {
-            base.emit('scrolled-to-top')
+            base.emit('scrolled-top')
         }
     }, PASSIVE)
 
