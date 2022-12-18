292309837b8667e27ea7298638c79634283cc180
Ardeshir Valipoor <ardeshir.valipoor@gmail.com>
2022-12-18 10:47:34 +0100

    state added

---
+++ services/state.ts
@@ -0,0 +1,32 @@
+let __state: any = {}
+
+function get(key: string, defaultValue = {}): any[] {
+    return __state[key] || defaultValue
+}
+
+function set(key: string, value: any) {
+    __state[key] = value
+}
+
+function merge(key: string, value: any) {
+    __state[key] = { ...__state[key], ...value }
+}
+
+function use(key: string, defaultValue = {}): IUseState {
+    if (!__state[key]) __state[key] = defaultValue
+    return [
+        () => __state[key], // get
+        (value: any) => __state[key] = value, // set
+        (value: any) => __state[key] = { ...__state[key], ...value } // merge
+        // Todo: handle nested merge (deep merge), example: { 'a.b.c.d': value }
+    ]
+}
+
+export default {
+    get,
+    set,
+    merge,
+    use,
+}
+
+type IUseState = [() => any, (value: any) => void, (value: any) => void]
