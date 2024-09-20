eb5f4b9a3eafae7501b462c25d1d4dc0075747d1
Ardeshir Valipoor <ardeshir.valipoor@gmail.com>
2024-09-20 11:55:17 +0200

    link updated

--- components/native/a.ts
+++ components/native/a.ts
@@ -1,12 +1,21 @@
 import { texter } from '../../utils/texter'
 import { Base } from '../base'
 
-export const A = (href: string, title: string, target = '_self') => {
+export const A = (href?: string, title?: string, target = '_self') => {
 
     const base = Base('a')
-    base.el.href = href
+    base.el.href = href || '#'
     base.el.setAttribute('target', target)
-    base.el.textContent = title
+    base.el.textContent = title || ''
 
-    return base
+    return Object.assign(base, {
+        setValue: (value: string) => {
+            base.el.textContent = value
+            if (value.startsWith('http')) base.el.href = value
+        },
+        setHref: (href: string) => base.el.href = href,
+        setTarget: (target: string) => base.el.setAttribute('target', target),
+        setDownload: (filename: string) => base.el.setAttribute('download', filename),
+    })
+
 }
